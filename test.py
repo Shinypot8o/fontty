c2d93cb40e298660b2b67e3b3aa0cbd539011e31
Shinypot8o <shinypot8o@slambodia.com>
2025-10-15 17:33:29 -0600

    reduce braille characters in unicode.map

--- test.py
+++ test.py
@@ -3,40 +3,47 @@ import sys
 
 bdf_file = sys.argv[1] if len(sys.argv) > 1 else "compactty.bdf"
 
-def is_in_font(c):
-	return (
-		c == 0
-		or 0x20 <= c < 0x7f
-		# or 0xa1 <= c < 0xff
-		or 0xb2 <= c < 0xb4
-		or c == 0xb9
-		or c == 0x2074
-		or 0x2190 <= c < 0x2194
-		or c == 0x21b5
-		or 0x2500 <= c < 0x2504
-		or 0x250c <= c < 0x254b
-		or 0x2550 <= c < 0x25a1
-		or c == 0x25b2
-		or c == 0x25bc
-		or 0x2800 <= c < 0x2900
-	)
 
-with open("unicode.map", "w") as f:
-	for c in range(0x10000):
-		if is_in_font(c):
-			f.write(f"U+{c:04X}\n")
+in_font = set()
+with open("unicode.map", "r") as f:
+	for line in f:
+		if line.startswith("U+"):
+			in_font.add(int(line[2:6], 16))
 
-if input("recompile font? (y/N) ").lower() == "y":
+# def is_in_font(c):
+# 	return (
+# 		c == 0
+# 		or 0x20 <= c < 0x7f
+# 		# or 0xa1 <= c < 0xff
+# 		or 0xb2 <= c < 0xb4
+# 		or c == 0xb9
+# 		or c == 0x2074
+# 		or 0x2190 <= c < 0x2194
+# 		or c == 0x21b5
+# 		or 0x2500 <= c < 0x2504
+# 		or 0x250c <= c < 0x254b
+# 		or 0x2550 <= c < 0x25a1
+# 		or c == 0x25b2
+# 		or c == 0x25bc
+# 		or 0x2800 <= c < 0x2900
+# 	)
+
+# with open("unicode.map", "r") as f:
+# 	for c in range(0x10000):
+# 		if is_in_font(c):
+# 			f.write(f"U+{c:04X}\n")
+
+if input("recompile font? (Y/n) ").lower() != "n":
 	os.system(f"sudo bdf2psf --fb {bdf_file} /dev/null unicode.map 512 {bdf_file[:-4]}.psf")
 if input("install font? (Y/n) ").lower() != "n":
 	os.system(f"sudo setfont {bdf_file[:-4]}.psf")
 
 for c in range(0x10000):
-	if is_in_font(c):
+	if c in in_font:
 		print(chr(c), end="")
 print()
 
 
 
-if input("\nrevert font? (Y/n) ").lower() != "n":
+if input("\nrevert font? (y/N) ").lower() == "y":
 	os.system("sudo setfont /usr/share/consolefonts/Lat2-Terminus16.psf.gz")
